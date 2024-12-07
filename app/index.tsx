a653b5fa50419dd70299db4e03949e2264fc09b9
Ariff Rahim <mohamedariff.abdrahim@gmail.com>
2024-12-07 12:52:14 +0800

    improve main page ui

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,10 @@
-import { SafeAreaView, StyleSheet } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import { Link } from 'expo-router'
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons'
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6'
 
 import CreditCard from '@/components/CreditCard'
+import TransactionHistory from '@/components/TransactionHistory'
 
 export default function Index() {
   return (
@@ -12,8 +15,37 @@ export default function Index() {
         expiryDate={'1/12'}
       />
 
-      <Link href="/transfer">Transfer Money</Link>
-      <Link href="/receipt">Receipt</Link>
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'baseline',
+          gap: 20,
+          marginBottom: 20,
+          paddingHorizontal: 20
+        }}
+      >
+        <Link href="/transfer">
+          <View style={{ alignItems: 'center' }}>
+            <MaterialCommunityIcons
+              name="bank-transfer"
+              size={40}
+              color="white"
+            />
+            <Text>Transfer</Text>
+          </View>
+        </Link>
+
+        <Link href="/receipt">
+          <View style={{ alignItems: 'center', gap: 5 }}>
+            <FontAwesome6 name="receipt" size={24} color="white" />
+            <Text>Receipt</Text>
+          </View>
+        </Link>
+      </View>
+
+      <View style={{ paddingHorizontal: 20 }}>
+        <TransactionHistory />
+      </View>
     </SafeAreaView>
   )
 }
@@ -21,7 +53,6 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     paddingHorizontal: 20,
     backgroundColor: 'silver'
   }
