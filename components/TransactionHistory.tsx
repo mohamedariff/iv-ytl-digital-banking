e7d87552016ab62f5559c69cd73747b4b33f5046
Ariff Rahim <mohamedariff.abdrahim@gmail.com>
2024-12-07 16:19:16 +0800

    improve transaction component

--- components/TransactionHistory.tsx
+++ components/TransactionHistory.tsx
@@ -1,48 +1,97 @@
+import type { Transaction } from '../store/bankAccount'
+
 import React from 'react'
 import { FlatList, Text, View } from 'react-native'
 
 import useBankAccountStore from '../store/bankAccount'
-
-import type { Transaction } from '../store/bankAccount'
+import { Link } from 'expo-router'
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons'
+import FontAwesome from '@expo/vector-icons/FontAwesome'
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6'
 
 type RenderListProps = {
   item: Transaction
 }
+
 function TransactionHistory() {
-  // const transactionHistory = useBankAccountStore.use.transactions()
-  const transactionHistory = [
-    {
-      transactionId: '1',
-      name: 'Mat Jargon',
-      account: '6090358170 - BCA',
-      amount: 'RM5,000.00',
-      status: 'success',
-      date: new Date().toISOString()
-    },
-    {
-      transactionId: '2',
-      name: 'Mat Arep',
-      account: '6090358170 - BCA',
-      amount: 'RM2,000.00',
-      status: 'failed',
-      date: new Date().toISOString()
-    }
-  ]
+  const transactionHistory = useBankAccountStore.use.transactions()
 
   const renderList = ({ item }: RenderListProps) => {
     return (
       <View
         style={{
-          backgroundColor: 'white',
+          padding: 10,
+          borderRadius: 5,
           flexDirection: 'row',
+          backgroundColor: 'white',
           justifyContent: 'space-between'
         }}
       >
-        <View>
-          <Text style={{ color: 'red' }}>{item.name}</Text>
-          <Text style={{ color: 'gray' }}>{item.account}</Text>
+        <View
+          style={{
+            flex: 0.7,
+            marginRight: 10,
+            alignItems: 'center',
+            flexDirection: 'row',
+            justifyContent: 'space-between'
+          }}
+        >
+          <View>
+            <Text style={{ color: 'gray' }}>{item.name}</Text>
+            <Text style={{ color: 'gray' }}>{item.account}</Text>
+          </View>
+
+          <View>
+            <Text style={{ color: 'green', fontWeight: '500', fontSize: 15 }}>
+              {new Intl.NumberFormat('ms-MY', {
+                style: 'currency',
+                currency: 'MYR'
+              }).format(item.amount)}
+            </Text>
+            <Text style={{ color: 'gray', fontSize: 13 }}>{item.date}</Text>
+          </View>
         </View>
-        <Text style={{ color: 'green' }}>{item.amount}</Text>
+
+        <View
+          style={{
+            flex: 0.3,
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            alignItems: 'center'
+          }}
+        >
+          <Link href="/transfer">
+            <View style={{ alignItems: 'center', gap: 5 }}>
+              <FontAwesome name="dollar" size={22} color="gray" />
+              <Text
+                style={{
+                  width: 40,
+                  fontSize: 10,
+                  flexWrap: 'wrap',
+                  textAlign: 'center'
+                }}
+              >
+                Quick Transfer
+              </Text>
+            </View>
+          </Link>
+
+          <Link href="/receipt">
+            <View style={{ alignItems: 'center', gap: 5 }}>
+              <FontAwesome6 name="receipt" size={24} color="gray" />
+              <Text
+                style={{
+                  width: 40,
+                  fontSize: 10,
+                  flexWrap: 'wrap',
+                  textAlign: 'center'
+                }}
+              >
+                Receipt
+              </Text>
+            </View>
+          </Link>
+        </View>
       </View>
     )
   }
